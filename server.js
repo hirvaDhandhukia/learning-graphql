b94b026455dccd9b32fb11160ee766584ce25793
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-02-11 19:44:53 +0530

    add: can see book info with it's author info

--- server.js
+++ server.js
@@ -27,16 +27,56 @@ const books = [
 	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
 ]
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            message: { 
-                type: GraphQLString,
-                resolve: () => 'Hello World Hirva'
-            }
-        })
+// const schema = new GraphQLSchema({
+//     query: new GraphQLObjectType({
+//         name: 'HelloWorld',
+//         fields: () => ({
+//             message: { 
+//                 type: GraphQLString,
+//                 resolve: () => 'Hello World Hirva'
+//             }
+//         })
+//     })
+// })
+const AuthorType = new GraphQLObjectType ({
+    name: 'Author',
+    description: 'This represnts the author of a book',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: {type: GraphQLNonNull(GraphQLString)}
     })
+})
+
+const BookType = new GraphQLObjectType ({
+    name: 'Book',
+    description: 'This represents a book written by an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: {type: GraphQLNonNull(GraphQLString)},
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
+        author: { 
+            type: AuthorType,
+            resolve: (book) => {
+                return authors.find(author => author.id === book.authorId)
+            }
+        }
+    })
+})
+
+const RootQueryType = new GraphQLObjectType ({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of All Books',
+            resolve: () => books
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
 })
 
 app.use('/graphql', expressGraphQL({
