60e2e213f362d4a78529040d725234ec647085c0
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-02-11 19:08:22 +0530

    create: new graphql server with express

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
+} = require('graphql')
 const app = express()
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: { 
+                type: GraphQLString,
+                resolve: () => 'HelloWorld'
+            }
+        })
+    })
+})
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}))
 app.listen(5000., ()=> console.log("Server is running"))
